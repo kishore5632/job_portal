d8b6b66ffd7e0bf303e6793500ca1dd97019abd8
kishore5632 <elkishore5@gmail.com>
2023-06-11 18:29:25 +0530

    updated settings.py file

--- alpha/alpha/settings.py
+++ alpha/alpha/settings.py
@@ -146,3 +146,6 @@ MEDIA_ROOT = BASE_DIR / "media/documents"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#update
